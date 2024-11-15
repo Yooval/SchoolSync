41b24c499272da072924a7e1117796b89982373e
Yooval <116361337+Yooval@users.noreply.github.com>
2024-11-15 14:25:11 +0200

    Initial Commit!

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppDummy } from './app.dummy';
-import { AppJapanService } from './app.japan.service';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import ormConfig from './config/orm.config';
 import ormConfigProd from './config/orm.config.prod';
